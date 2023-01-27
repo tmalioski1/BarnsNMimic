122310fd56c9bdc2cb35e6a8c83cd77a6c44a69b
Tyler Malinoski <tmalinoski1@gmail.com>
2023-01-26 21:21:50 -0800

    lots of changes

---
+++ react-app/src/components/NavBar/index.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {useSelector} from 'react-redux'
+import OpenModalButton from '../OpenModalButton'
+import LogoutButton from '../auth/LogoutButton';
+import UploadNewBook from '../UploadBookModal'
+
+import './navbar.css'
+
+const NavBar = ({loaded}) => {
+  const sessionUser = useSelector(state => state.session.user);
+  let sessionLinks;
+
+  if(sessionUser) {
+    sessionLinks = (
+     <>
+      <nav className='nav'>
+      <div className='header'>
+      <NavLink className={'homeLink'} to='/' exact={true} activeClassName='active' >
+          <div className='homelink-container'>
+            <div className='homeWord'>
+              <p className='word'>Barnes & Noble</p>
+            </div>
+          </div>
+        </NavLink>
+        <div className='header-center'>
+            <input
+            type='text'
+            disabled
+            placeholder='Search coming soon'
+            className='search-input'>
+            </input>
+            <div className='search-icon'>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+          </div>
+          <div className='header-right'>
+            <div className='upload-button'>
+            <OpenModalButton
+            buttonText='Upload'
+            modalComponent={<UploadNewBook />}
+            />
+            </div>
+            <div className='log-out'>
+            <LogoutButton />
+            </div>
+        </div>
+        </div>
+      </nav>
+     </>
+    )
+   } else {
+    sessionLinks = (
+      <>
+      <nav className='nav'>
+        <div className='header'>
+          <NavLink className={'homeLink'} to='/' exact={true} activeClassName='active'>
+            <div className='homelink-container'>
+              <div className='homeWord'>
+                <p className='word'>Barnes & Noble</p>
+              </div>
+            </div>
+          </NavLink>
+          <div className='header-center-logged-out'>
+            <input
+              type='text'
+              disabled
+              placeholder='Feature coming soon'
+              className='search-input'>
+            </input>
+            <div className='search-icon'>
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+          </div>
+          <div className='header-right'>
+            <div className='login-nav'>
+              <NavLink className='login-nav' to='/login' exact={true} activeClassName='active'>
+                Login
+              </NavLink>
+            </div>
+            <div className='sign-up-nav'>
+              <NavLink className='sign-up-nav' to='/sign-up' exact={true} activeClassName='active'>
+                Sign Up
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+      </>
+    )
+   }
+
+   return (
+    <div>
+      {loaded && sessionLinks}
+    </div>
+  );
+}
+
+export default NavBar;
