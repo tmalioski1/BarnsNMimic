66a9db2c8dfa4d19d976b0a86e54d05732cebea0
Tyler Malinoski <tmalinoski1@gmail.com>
2023-01-27 09:54:58 -0800

    debugged signup

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -5,6 +5,8 @@ import { signUp } from '../../store/session';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -15,12 +17,21 @@ const SignUpForm = () => {
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
+      const data = await dispatch(signUp(first_name, last_name, username, email, password));
       if (data) {
         setErrors(data)
       }
     }
   };
+
+  const updateFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const updateLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
 
   const updateUsername = (e) => {
     setUsername(e.target.value);
@@ -49,6 +60,28 @@ const SignUpForm = () => {
           <div key={ind}>{error}</div>
         ))}
       </div>
+      <div id='first-name-signup'>
+        <label id='firstname-label-signup'>First Name</label>
+        <input
+          type='text'
+          name='First Name'
+          required
+          onChange={updateFirstName}
+          id='first-name-input-field-login'
+          value={first_name}
+        ></input>
+      </div>
+      <div id='last-name-signup'>
+        <label id='lastname-label-signup'>Last Name</label>
+        <input
+          type='text'
+          name='Last Name'
+          required
+          onChange={updateLastName}
+          id='last-name-input-field-login'
+          value={last_name}
+        ></input>
+      </div>
       <div>
         <label>User Name</label>
         <input
@@ -63,6 +96,7 @@ const SignUpForm = () => {
         <input
           type='text'
           name='email'
+          required
           onChange={updateEmail}
           value={email}
         ></input>
