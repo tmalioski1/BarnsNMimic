d8931a1b25c4ff2ca0ca1b67391fb5bd887a6412
Tyler Malinoski <tmalinoski1@gmail.com>
2023-01-31 19:19:21 -0800

    changes again

--- app/forms/book_form.py
+++ app/forms/book_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField, FloatField
-from wtforms.validators import DataRequired, Optional, ValidationError
+from wtforms.validators import DataRequired
 
 
 
