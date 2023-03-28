d1d8fe52df48a07f01c8a6dc4a1cb9f097d982b5
Tyler Malinoski <tmalinoski1@gmail.com>
2023-03-27 20:52:38 -0700

    k

--- app/forms/cart_item_form.py
+++ app/forms/cart_item_form.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
+from wtforms import IntegerField, SubmitField, FloatField
 from wtforms.validators import DataRequired
 
 class CartItemForm(FlaskForm):
     quantity = IntegerField("Quantity", validators=[DataRequired()])
+    price= FloatField("Price", validators=[DataRequired()])
     submit = SubmitField("Update Quantity")
-    ##price##
